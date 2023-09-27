7a937291ef6e287113f832c6193e1b875456a9c4
Jean-David Daviet <jean-david.daviet@adeliom.com>
2023-09-27 17:05:00 +0200

    build(deps): add tailwindcss

--- assets/app.tsx
+++ assets/app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './styles/app.css';
 
-const root = createRoot(document.getElementById('app'));
+const root = createRoot(document.getElementById('app') as HTMLDivElement);
 root.render(<h1>Hello, world</h1>);
