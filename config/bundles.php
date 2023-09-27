857d42415f08b4fa84684116b7cd7b654f1cd0b3
Jean-David Daviet <jean-david.daviet@adeliom.com>
2023-09-27 17:01:08 +0200

    build(deps): add webpack encore

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Adeliom\EasyCommonBundle\EasyCommonBundle::class => ['all' => true],
     Adeliom\EasyAdminUserBundle\EasyAdminUserBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
