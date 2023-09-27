2c7245fa242eff06c1181986bf667d7d4fb84e5b
Jean-David Daviet <jean-david.daviet@adeliom.com>
2023-09-27 17:02:21 +0200

    build(deps): add reactjs

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,5 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello, world</h1>);
